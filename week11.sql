b054bf665b18f89855cb49c11d8a58e6affc7e16
Olatundun19 <113788919+Olatundun19@users.noreply.github.com>
2024-11-14 12:05:27 -0500

    Update week11.sql

--- week11.sql
+++ week11.sql
@@ -1,1 +1,22 @@
+SELECT title, due_date
+FROM assignments
+WHERE course_id LIKE 'COMP1234'
 
+SELECT min(due_date)
+FROM assignments
+
+SELECT max(due_date)
+FROM assignments
+
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08'
+
+SELECT title, due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%'
+
+
+SELECT max(due_date)
+FROM assignments
+WHERE status LIKE 'completed'
