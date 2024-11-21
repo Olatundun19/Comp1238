b3e9164470ad6888e20470511deb6abef7f5806a
Olatundun19 <113788919+Olatundun19@users.noreply.github.com>
2024-11-20 20:56:31 -0500

    Update lab12.sql

--- lab12.sql
+++ lab12.sql
@@ -1,4 +1,7 @@
--- Lines starting with double dash are ignored by the DB
+SELECT concat(course_name, '-', semester)
+FROM courses
+
+  
 SELECT   course_id, course_name, lab_time
 FROM courses
 WHERE lab_time LIKE 'Friday' ;
@@ -10,3 +13,21 @@ WHERE due_date> '2024-11-20'
 SELECT status, COUNT(*) assignments_count
 FROM assignments
 GROUP BY  status;
+
+SELECT course_name
+AS length_name
+FROM courses
+ORDER by length_name DESC
+LIMIT 1;
+
+SELECT upper(course_name)
+AS uppercase
+FROM courses
+
+SELECT title
+FROM assignments
+WHERE due_date LIKE '%-09-%'
+
+SELECT *
+FROM assignments
+WHERE due_date ISNULL
